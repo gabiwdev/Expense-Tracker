0882559be10acbe715c0ffcfa248f68d864217cb
Dell <gabiwoutersm@gmail.com>
2026-02-10 10:25:27 -0300

    Inicio da Função detectar_moeda - Inicio da função pra detectar o gasto e em qual moeda - atualmente separa a parte da string que tiver um algarismo (incluindo "23R$")

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import datetime as dt
 import string
 import unicodedata as ucd
+import re
 
 
 
 def limpar(palavra):
+
+    pontuacao = ',<>´`".%&*()'
+
     palavra = ''.join(
         c for c in ucd.normalize('NFD', palavra)
         if ucd.category(c) != 'Mn'
@@ -12,8 +16,19 @@ def limpar(palavra):
 
     palavra = palavra.lower()
     texto = palavra.split()
-    texto = [t.strip(string.punctuation) for t in texto]
+    texto = [t.strip(pontuacao) for t in texto]
 
     return ' '.join(texto)
 
 
+def detectar_moeda(frase):
+    números = []
+    texto = frase.split()
+    for palavra in texto:
+        if re.search(r'\d', palavra):
+            números.append(palavra)
+    print(números)
+
+tst = limpar('comprei duas coxinha ontem, foi 23R$.')
+
+detectar_moeda(tst)
